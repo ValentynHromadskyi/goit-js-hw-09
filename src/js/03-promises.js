f1a278605da0fec0a067fd6bc0351399b61d565f
ValentynHromadskyi <126354727+ValentynHromadskyi@users.noreply.github.com>
2023-11-12 20:06:00 +0200

    fixed bugs

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,23 +7,22 @@ const refs = {
   step: document.querySelector('input[name="step"]'),
   amount: document.querySelector('input[name="amount"]'),
   body: document.querySelector('body'),
-  input: document.querySelector('label')
 };
-
-
-let i=0;
-let amount=0;
-let step=0;
+let timeId;
+let timerButton;
+let i = 0;
+let amount = 0;
+let step = 0;
 refs.form.addEventListener('submit', onSubmit);
 refs.button.disabled = false;
-    
+
 function onSubmit(event) {
   event.preventDefault();
   amount = refs.amount.value;
   step = Number(refs.step.value);
   let delay = Number(refs.delay.value);
   let position = 0;
-  for (i ; i < amount; i += 1) {
+  for (i; i < amount; i += 1) {
     position += 1;
 
     createPromise(position, delay)
@@ -39,14 +38,14 @@ function onSubmit(event) {
       });
     delay += step;
   }
-  a = setTimeout(() => {
+  timerButton = setTimeout(() => {
     refs.button.disabled = false;
   }, delay);
-  position=0;
-  i=0;
+  position = 0;
+  i = 0;
   refs.button.disabled = true;
   refs.form.reset();
-};
+}
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
@@ -59,11 +58,11 @@ function createPromise(position, delay) {
       }
     }, delay);
   });
-};
+}
 
 function style() {
   refs.body.style.backgroundColor = 'MediumSeaGreen';
   refs.form.style.cssText =
     'position: absolute;top: 50%; left: 50%;transform: translateX(-50%) translateY(-50%);color: black ;font-size: 20px;letter-spacing: 2px;display: flex; gap: 30px;';
- }
+}
 style();
